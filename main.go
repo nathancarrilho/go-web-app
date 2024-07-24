8673216cd9876b3cdd9a112cc5b789874b2c4c1d
nathancarrilho <nathancarrilholeal@gmail.com>
2024-07-24 15:42:59 +0100

    Update html

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/home.html")
 }
 
-func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
+func certificationsPage(w http.ResponseWriter, r *http.Request) {
+	// Render the certifications html page
+	http.ServeFile(w, r, "static/certifications.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
@@ -28,7 +28,7 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
+	http.HandleFunc("/certifications", certificationsPagePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
 
